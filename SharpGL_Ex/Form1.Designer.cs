627d4b78c28ed6e1eb627c81ad42c47eccfae667
CuteTN <quantiennghia001@gmail.com>
2020-12-14 11:23:48 +0700

    Apply exercise template

---
+++ SharpGL_Ex/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace SharpGL_Ex
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openGLControl = new SharpGL.OpenGLControl();
+            this.button_Line = new System.Windows.Forms.Button();
+            this.button_Circle = new System.Windows.Forms.Button();
+            this.button_Color = new System.Windows.Forms.Button();
+            this.button_Rectangle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl
+            // 
+            this.openGLControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.openGLControl.DrawFPS = false;
+            this.openGLControl.Location = new System.Drawing.Point(207, 13);
+            this.openGLControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.openGLControl.Name = "openGLControl";
+            this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl.Size = new System.Drawing.Size(580, 424);
+            this.openGLControl.TabIndex = 0;
+            this.openGLControl.OpenGLInitialized += new System.EventHandler(this.openGLControl_OpenGLInitialized);
+            this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl_OpenGLDraw);
+            this.openGLControl.Resized += new System.EventHandler(this.openGLControl_Resized);
+            this.openGLControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.openGLControl_MouseDown);
+            this.openGLControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.openGLControl_MouseMove);
+            this.openGLControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.openGLControl_MouseUp);
+            // 
+            // button_Line
+            // 
+            this.button_Line.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Line.Location = new System.Drawing.Point(12, 59);
+            this.button_Line.Name = "button_Line";
+            this.button_Line.Size = new System.Drawing.Size(188, 40);
+            this.button_Line.TabIndex = 1;
+            this.button_Line.Text = "Line";
+            this.button_Line.UseVisualStyleBackColor = true;
+            this.button_Line.Click += new System.EventHandler(this.button_Line_Click);
+            // 
+            // button_Circle
+            // 
+            this.button_Circle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Circle.Location = new System.Drawing.Point(12, 105);
+            this.button_Circle.Name = "button_Circle";
+            this.button_Circle.Size = new System.Drawing.Size(188, 40);
+            this.button_Circle.TabIndex = 2;
+            this.button_Circle.Text = "Circle";
+            this.button_Circle.UseVisualStyleBackColor = true;
+            this.button_Circle.Click += new System.EventHandler(this.button_Circle_Click);
+            // 
+            // button_Color
+            // 
+            this.button_Color.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Color.Location = new System.Drawing.Point(12, 13);
+            this.button_Color.Name = "button_Color";
+            this.button_Color.Size = new System.Drawing.Size(188, 40);
+            this.button_Color.TabIndex = 3;
+            this.button_Color.Text = "Color";
+            this.button_Color.UseVisualStyleBackColor = true;
+            this.button_Color.Click += new System.EventHandler(this.button_Color_Click);
+            // 
+            // button_Rectangle
+            // 
+            this.button_Rectangle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Rectangle.Location = new System.Drawing.Point(12, 151);
+            this.button_Rectangle.Name = "button_Rectangle";
+            this.button_Rectangle.Size = new System.Drawing.Size(188, 40);
+            this.button_Rectangle.TabIndex = 4;
+            this.button_Rectangle.Text = "Rectangle";
+            this.button_Rectangle.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Rectangle);
+            this.Controls.Add(this.button_Color);
+            this.Controls.Add(this.button_Circle);
+            this.Controls.Add(this.button_Line);
+            this.Controls.Add(this.openGLControl);
+            this.DoubleBuffered = true;
+            this.Name = "FormMain";
+            this.Text = "SharpGL Ex";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl;
+        private System.Windows.Forms.Button button_Line;
+        private System.Windows.Forms.Button button_Circle;
+        private System.Windows.Forms.Button button_Color;
+        private System.Windows.Forms.Button button_Rectangle;
+    }
+}
+
---
+++ SharpGL_Ex/Form1.cs
@@ -0,0 +1,198 @@
+﻿using SharpGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpGL_Ex
+{
+    // using this type to know what user is currently drawing
+    enum FormMode
+    {
+        Normal,
+        DrawLine,
+        DrawCircle,
+        DrawRectangle,
+    }
+
+    public partial class FormMain : Form
+    {
+        #region Utils stuff
+        FormMode formMode = FormMode.Normal;
+
+        Point mouseDownPosition = new Point(-1,-1);
+        Point mouseUpPosition = new Point(-1,-1);
+        bool flagMouseIsDown = false;
+
+        Color chosenColor = Color.LightPink; // dont get and set directly to this
+        Color ChosenColor
+        {
+            get => chosenColor;
+            set
+            {
+                chosenColor = value;
+
+                button_Color.BackColor = value;
+
+                // if this is a bright color
+                if(value.R + value.G + value.B >= 128*3)
+                    button_Color.ForeColor = Color.Black;
+                else
+                    button_Color.ForeColor = Color.White;
+            }
+        }
+        #endregion
+
+        #region Init stuff
+        private void CustomInit()
+        {
+            ChosenColor = Color.LightPink;
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+            CustomInit();
+        }
+        #endregion
+
+        #region Template stuff
+        private void openGLControl_OpenGLInitialized(object sender, EventArgs e)
+        {
+            // Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+            // Set the clear color.
+            gl.ClearColor(0, 0, 0, 0);
+            // Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            // Load the identity.
+            gl.LoadIdentity();
+        }
+
+        private void openGLControl_Resized(object sender, EventArgs e)
+        {
+            // Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+            // Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            // Load the identity.
+            gl.LoadIdentity();
+            // Create a perspective transformation.
+            gl.Viewport(0, 0, openGLControl.Width, openGLControl.Height);
+            gl.Ortho2D(0, openGLControl.Width, 0, openGLControl.Height);
+        }
+
+        private void openGLControl_OpenGLDraw(object sender, RenderEventArgs args)
+        {
+            // Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+            // Clear the color and depth buffer.
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            gl.Color(ChosenColor.R, ChosenColor.G, ChosenColor.B);
+
+            // Vẽ vời chỗ này. Ví dụ:
+            //gl.Begin(OpenGL.GL_TRIANGLES); // Chọn chế độ vẽ tam giác
+            //gl.Vertex2sv(new short[] { 0, 0 }); // Đỉnh thứ 1 tọa độ 0,0
+            //gl.Vertex2sv(new short[] { 100, 100 }); // Đỉnh thứ 2 tọa độ 100, 100
+            //gl.Vertex2sv(new short[] { 200, 0 }); // Đỉnh thứ 3 tọa độ 200, 0
+            //gl.End();
+            //gl.Flush(); // Thực hiện lệnh vẽ ngay lập tức thay vì đợi sau 1 khoảng thời gian
+
+            // CuteTN note: add new shapes cases here
+            switch(formMode)
+            {
+                case FormMode.DrawLine:
+                    DrawLine(gl);
+                    break;
+
+                case FormMode.DrawCircle:
+                    DrawCircle(gl);
+                    break;
+
+                case FormMode.DrawRectangle:
+                    DrawRectangle(gl);
+                    break;
+
+                default:
+                    break;
+            }
+
+            gl.Flush();
+        }
+
+        #endregion
+
+        #region Buttons Handling stuff
+        private void button_Color_Click(object sender, EventArgs e)
+        {
+            // open color picker form
+            ColorDialog dlg = new ColorDialog();
+            var dlgRes = dlg.ShowDialog();
+
+            if(dlgRes == DialogResult.OK)
+            {
+                ChosenColor = dlg.Color;
+            }
+        }
+
+        private void button_Line_Click(object sender, EventArgs e)
+        {
+            formMode = FormMode.DrawLine;
+        }
+
+        private void button_Circle_Click(object sender, EventArgs e)
+        {
+            formMode = FormMode.DrawCircle;
+        }
+        #endregion
+
+        #region Mouse handling stuff
+        private void openGLControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDownPosition = e.Location;
+            mouseUpPosition = e.Location;
+            flagMouseIsDown = true;
+        }
+
+        private void openGLControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(flagMouseIsDown)
+                mouseUpPosition = e.Location;
+        }
+
+        private void openGLControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            flagMouseIsDown = false;
+
+            // CuteTN note: add Shape to shape collection here
+        }
+        #endregion
+
+        #region Shapes drawing stuff
+        // CuteTN note: we gonna need some better OOP stuff and polymorphism pla pla here :)
+        // CuteTN note: add drawing logic to the functions below
+
+        void DrawLine(OpenGL gl)
+        {
+            gl.Begin(OpenGL.GL_LINES);
+            gl.Vertex(mouseDownPosition.X, gl.RenderContextProvider.Height - mouseDownPosition.Y);
+            gl.Vertex(mouseUpPosition.X, gl.RenderContextProvider.Height - mouseUpPosition.Y);
+            gl.End();
+        }
+
+        void DrawCircle(OpenGL gl)
+        {
+        }
+
+        void DrawRectangle(OpenGL gl)
+        {
+        }
+        #endregion
+    }
+}
